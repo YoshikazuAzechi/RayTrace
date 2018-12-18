849a1028d527cf477680146b2ff5d175f7f6c175
Yoshikazu Azechi <walkingcat.cute@gmail.com>
2018-12-19 02:02:15 +0900

    add chapter03

--- RayTracingInOneWeekend/Chapter03/chapter03.cpp
+++ RayTracingInOneWeekend/Chapter03/chapter03.cpp
@@ -4,10 +4,12 @@
 #include <iostream>
 #include <cstdint>
 
-#include "vector3.h"
+#include "ray.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "../stb_image_write.h"
+
+Vector3 SkyColor(const Ray& ray);
 
 void main()
 {
@@ -18,14 +20,47 @@ void main()
     
     uint8_t* buffer = new uint8_t[nx * ny * components];
     
+    // スクリーンサイズ
+    Vector3 screenWidth(4.0f, 0.0f, 0.0f);
+    Vector3 screenHeight(0.0f, 2.0f, 0.0f);
+    Vector3 screenLowerLeft = -(screenWidth / 2.0f + screenHeight / 2.0f);
+    // カメラ位置
+    Vector3 cameraPosition(0.0f, 0.0f, 0.0f);
+    
     // 各ピクセルに色を付ける
     for(int j = ny - 1;j >= 0;--j)
     {
         for(int i = 0;i < nx;++i)
         {
+            // UV値
+            float u = float(i) / float(nx);
+            float v = float(j) / float(ny);
+            
+            // Ray(camera)
+            Vector3 cameraForward = screenLowerLeft + screenWidth * u + screenHeight * v;
+            Ray ray(cameraPosition, cameraForward);
+            
+            // SkyColor
+            Vector3 color = SkyColor(ray);
+            
+            int ir = int(255.99f * color.R());
+            int ig = int(255.99f * color.G());
+            int ib = int(255.99f * color.B());
+            
+            // 色を出力
+            int index = i * components + j * (nx * components);
+            buffer[index] = ir;
+            buffer[index+1] = ig;
+            buffer[index+2] = ib;
         }
     }
     
-    //stbi_write_jpg("Chapter03/chapter03.jpg", nx, ny, components, buffer, 0);
+    stbi_write_jpg("Chapter03/chapter03.jpg", nx, ny, components, buffer, 0);
     delete[] buffer;
+}
+
+Vector3 SkyColor(const Ray& ray)
+{
+    float t = 0.5f * (1.0f  + ray.direction.Normalized().Y());
+    return Vector3(1.0f, 1.0f, 1.0f) * (1.0f - t) + Vector3(0.5f, 0.7f, 1.0f) * t;
 }
